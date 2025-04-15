339b0e13e2a099d55972d491c785aea80ffff8a1
Rizwan Hasan <rizwan.hasan486@gmail.com>
2025-04-16 00:38:24 +0600

    added bun and formatted

--- app/routes/country.tsx
+++ app/routes/country.tsx
@@ -4,7 +4,7 @@ export async function clientLoader({ params }: Route.LoaderArgs) {
   const countryName = params.countryName;
 
   const res = await fetch(
-    `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
+    `https://restcountries.com/v3.1/name/${countryName}?fullText=true`,
   );
   const data = await res.json();
   return data;
